563ad18b93e8ffcf75744ac20bc5f8932b233c88
john0017 <63656509+john0017@users.noreply.github.com>
2021-10-21 14:01:13 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, url_for, request, abort, redirect, session, flash, send_file, make_response, jsonify, Response
+from flask_bootstrap import Bootstrap
+import pandas as pd
+import mysql.connector
+import uuid
+from functools import wraps
+from datetime import timedelta, date, datetime
+import cv2
+from pyzbar.pyzbar import decode
+from flask_socketio import SocketIO, emit, disconnect
+from PIL import Image
+from io import BytesIO
+import base64
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+Bootstrap(app)
+socketio = SocketIO(app, logger=True, engineio_logger=True)
+
+@app.route('/', methods=['GET', 'POST'])
+def vid():
+    return render_template("scanner.html",
+                        )
+
+@socketio.on('connect')
+def connect():
+    print("client connected")
+
+@socketio.on('message')
+def message(msg):
+    print("\n\nmessage: ", msg['data'])
+
+@socketio.on('process')
+def test_connect(data):
+    print(data)
+    emit('test echo', 'echoed')
+    disconnect(request.sid)
+
+
+socketio.run(app)
